428a57effbf2f8ed0382769ab809c23d98966185
ViperDish <samp_037r2@mail.ru>
2022-11-10 22:17:49 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+
+{
+    public class Delegate3
+    {
+        public class Acc
+        {
+            public delegate void AccountHandler(string message);
+            AccountHandler taken;
+            public event AccountHandler Notify;
+
+            public int Sum { get; private set; }
+            string Name;
+
+            public Acc(int Sum, string Name)
+            {
+                this.Sum = Sum;
+                this.Name = Name;
+            }
+            public void RegisterHandler(AccountHandler del)
+            {
+                taken = del;
+            }
+            public void Info(int sum, string name)
+            {
+                Notify?.Invoke($"ФИО Пользователя: {name}, баланс: {sum}");
+                taken?.Invoke($"ФИО Пользователя: {name}, баланс: {sum}");
+            }
+            public void Add(int sum)
+            {
+                Sum += +sum;
+                Notify?.Invoke($"на счёт поступило {sum}, баланс {Sum}");
+                taken?.Invoke($"на счёт поступило {sum}, баланс {Sum}");
+            }
+            public void Take(int sum)
+            {
+                if (this.Sum >= Sum)
+                {
+                    this.Sum -= sum;
+                    Notify?.Invoke($"со счёта списано {sum}, баланс {Sum}");
+                    taken?.Invoke($"со счёта списано {sum}, баланс {Sum}");
+                }
+                else
+                {
+                    Notify?.Invoke($"Недостаточно средств. Текущий баланс:{this.Sum}");
+                    taken?.Invoke($"Недостаточно средств. Текущий баланс:{this.Sum}");
+                }
+            }
+
+        }
+    }
+
+}
---
+++ SBERMBANK/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+
+namespace SBERMBANK
+{
+    public partial class Form1 : Form
+    {
+        void Display(string message)
+        {
+            MessageBox.Show(message);
+        }
+        void Print(string message)
+        {
+            listBox1.Items.Add(message);
+        }
+        Delegate3.Acc one;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            one = new Delegate3.Acc(Convert.ToInt32(textBox2.Text), textBox1.Text);
+            one.Notify += Display;
+            one.RegisterHandler(Print);
+            one.Info(Convert.ToInt32(textBox2.Text), textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            one.Add(Convert.ToInt32(textBox3.Text));
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            one.Take(Convert.ToInt32(textBox3.Text));
+        }
+    }
+}
